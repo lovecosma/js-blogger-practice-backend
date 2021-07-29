69e8e0f2e16615c095818cf2c7d9fab4a83b6571
Matthew Williams <love.cosma@gmail.com>
2021-07-28 20:59:35 -0600

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+
+    def index
+        posts = Post.all
+        render json: posts
+    end 
+
+    def show
+        post = Post.find(params[:id])
+        if post 
+            render json: post
+        else
+            render json: {error: "post not found"}
+        end 
+    end 
+
+    def create
+        post = Post.new(post_params)
+
+        if post.save
+            render json: post
+        else
+            binding.pry
+        end 
+
+    end 
+
+    def update
+        post = Post.find(params[:id])
+
+        if post.update(post_params)
+            render json: post
+        else
+            binding.pry
+        end 
+
+    end 
+
+    def destroy
+
+        post = Post.find(params[:id])
+        post.destroy
+        render json: Post.all
+    end 
+
+    private 
+
+    def post_params
+
+        params.require(:post).permit(:title, :content, :likes)
+
+    end 
+
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Post.create(title: "My first post!" , content: "This is a really great post", likes: 0)
+Post.create(title: "My second post!", content: "this is an ok post", likes: 0)
+Post.create(title: "MY THIRD POST", content: "THIS IS A VERY EXCITING POST", likes: 0)
+Post.create(title: "my fourth post", content: "meh", likes: 0)
+Post.create(title: "My fifth post!", content: "This is a very fun post", likes: 0)
+Post.create(title: "My sixth post", content: "This is a very sixy post", likes: 0)
+Post.create(title: "My seventh post", content: "I don't think I want to post anymore", likes: 0)
+Post.create(title: "My 8th post!", content: "I'm going to be the best poster ever", likes: 0)
